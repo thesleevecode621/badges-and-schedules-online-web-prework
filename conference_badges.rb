9ce0a69734eb00cffa9764962af83e4c247aad0b
boruch d <dubywider@gmail.com>
2019-04-02 14:45:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+def batch_badge_creator(attendees)
+  badge_messages = []
+
+   attendees.each do |person|
+    badge_messages << badge_maker(person)
+  end
+  badge_messages
+end
